50927c1acba7ea56cad0462138d4e0bb707f7248
a-azizweb <azizahmadhaider@gmail.com>
2023-01-27 21:35:20 +0500

    removing error

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,3 +1,13 @@
+// class ErrorHandler extends Error {
+//   constructor(message, statusCode) {
+//     super(message);
+//     this.statusCode = statusCode;
+
+//     Error.captureStackTrace(this, this.constructor);
+//   }
+// }
+// module.exports = ErrorHandler;
+
 class ErrorHandler extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -6,4 +16,5 @@ class ErrorHandler extends Error {
     Error.captureStackTrace(this, this.constructor);
   }
 }
+
 module.exports = ErrorHandler;
