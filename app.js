2fd2ec659e658a69c3bee78d96555a817aaeca26
a-azizweb <azizahmadhaider@gmail.com>
2023-01-27 23:12:57 +0500

    updaing app.js

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: '25mb' }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://ecommerce-0rgq.onrender.com'],
+  })
+);
 
 app.use(fileUpload());
 
