c0ff48675c7221ea91b992dc83d779ea5dfbfee1
a-azizweb <azizahmadhaider@gmail.com>
2023-01-28 11:08:47 +0500

    adding cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const userRoute = require('./routes/userRoute');
 const orderRoute = require('./routes/orderRoute');
 const payment = require('./routes/paymentRoute');
 const dotenv = require('dotenv');
-// var cors = require('cors');
+var cors = require('cors');
 const app = express();
 
 // app.use(express.json());
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: '25mb' }));
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: ['http://localhost:3000', 'https://ecommerce-0rgq.onrender.com'],
@@ -33,10 +34,10 @@ app.use(fileUpload());
 
 //this means ('/api/v1/products',(req,res)=>{})
 //the '/proucts' and the callback fnc is imported from productRoute.js file
-app.use('/api/v1', productRoute);
-app.use('/api/v1', userRoute);
-app.use('/api/v1', orderRoute);
-app.use('/api/v1', payment);
+app.use('https://ecommerce-0rgq.onrender.com/api/v1', productRoute);
+app.use('https://ecommerce-0rgq.onrender.com/api/v1', userRoute);
+app.use('https://ecommerce-0rgq.onrender.com/api/v1', orderRoute);
+app.use('https://ecommerce-0rgq.onrender.com/api/v1', payment);
 
 app.use(errorMiddleware);
 module.exports = app;
