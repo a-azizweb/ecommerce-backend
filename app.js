886e1005f7ccf0f2d28d581a8accf8397eaf3f62
a-azizweb <azizahmadhaider@gmail.com>
2023-01-28 11:22:36 +0500

    updating app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: '25mb' }));
 // app.use(cors());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://ecommerce-0rgq.onrender.com'],
+    origin: ['http://localhost:4000', 'https://ecommerce-0rgq.onrender.com'],
   })
 );
 
