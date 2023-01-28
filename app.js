ce51a28830045fefaa222bb7bced421963697a3f
a-azizweb <azizahmadhaider@gmail.com>
2023-01-28 11:16:25 +0500

    adding cors

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: '25mb' }));
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: ['http://localhost:3000', 'https://ecommerce-0rgq.onrender.com'],
-//   })
-// );
+// app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://ecommerce-0rgq.onrender.com'],
+  })
+);
 
 app.use(fileUpload());
 
